e3d5700439f30a7d95ec811de68facf3ba9cdf56
Emma V <emzvog@gmail.com>
2017-10-23 12:47:14 +0200

    Yaada

---
+++ includes/DbP.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package: Yadda
+ * Authors: Emma, Tiago, Michal and Lene
+ *
+ */
+
+abstract class DbP {
+    const DBHOST = 'localhost';
+    const DBUSER = 'root';
+    const USERPWD = '';
+    const DB = 'yadda';
+}
